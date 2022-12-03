b8bb220fb0828ae88dcefeca5cadb1f5693add55
Sefa Sertkaya <sefqi52@gmail.com>
2022-12-03 15:59:16 +0300

    tarih

--- store/models.py
+++ store/models.py
@@ -60,7 +60,7 @@ class Product(models.Model):
     tags          = models.CharField(max_length=400, blank=True)
     description   = RichTextField(blank=True)
     is_available  = models.BooleanField(default=False)
-    created_date  = models.DateTimeField(auto_now_add=True)
+    created_date  = models.DateField()
     modified_date = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
