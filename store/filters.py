10d60c46c40edd59700949370990a08706b45339
Sefa Sertkaya <sefqi52@gmail.com>
2022-12-14 16:38:41 +0300

    kartlar

--- store/filters.py
+++ store/filters.py
@@ -3,9 +3,6 @@ import django_filters
 from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
-    price_gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
-    price_lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
-
     class Meta:
         model = Product
         fields = ["category", "brand"]
